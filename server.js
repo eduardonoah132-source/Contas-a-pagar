0d438701e9c568efc5290090ed7f1a4baedd13c9
Eduardo Souza da Silva <eduardonoah132@gmail.com>
2026-05-02 10:02:00 -0300

    primeira versão do app

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
@@ -14,39 +13,59 @@ const db = mysql.createConnection({
   database: 'contas_app'
 });
 
-app.get('/contas',(req,res)=>{
-  db.query('SELECT * FROM contas',(err,result)=>{
-    if(err) throw err;
+// 🔥 IMPORTANTE: conectar no banco
+db.connect(err => {
+  if (err) {
+    console.error('Erro ao conectar no MySQL:', err);
+    return;
+  }
+  console.log('MySQL conectado');
+});
+
+// ROTAS
+app.get('/contas', (req, res) => {
+  db.query('SELECT * FROM contas', (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro no banco');
+    }
     res.json(result);
   });
 });
 
-app.post('/contas',(req,res)=>{
-  const {nome,valor,vencimento,tipo} = req.body;
-  db.query('INSERT INTO contas (nome,valor,vencimento,tipo,pago) VALUES (?,?,?,?,false)',
-  [nome,valor,vencimento,tipo],(err,result)=>{
-    if(err) throw err;
-    res.json(result);
-  });
+app.post('/contas', (req, res) => {
+  const { nome, valor, vencimento, tipo } = req.body;
+
+  db.query(
+    'INSERT INTO contas (nome, valor, vencimento, tipo, pago) VALUES (?, ?, ?, ?, false)',
+    [nome, valor, vencimento, tipo],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro ao inserir');
+      }
+      res.json(result);
+    }
+  );
 });
 
-app.put('/contas/:id',(req,res)=>{
+app.put('/contas/:id', (req, res) => {
   const id = req.params.id;
-  db.query('UPDATE contas SET pago = NOT pago WHERE id=?',[id],(err,result)=>{
-    if(err) throw err;
-    res.json(result);
-  });
+
+  db.query(
+    'UPDATE contas SET pago = NOT pago WHERE id = ?',
+    [id],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro ao atualizar');
+      }
+      res.json(result);
+    }
+  );
 });
 
-app.listen(3000, () => console.log('Servidor rodando'));
-*/
-console.log('SERVIDOR INICIADO');
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('OK');
+// START
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
 });
-
-app.listen(3000, () => console.log('Rodando na porta 3000'));   
